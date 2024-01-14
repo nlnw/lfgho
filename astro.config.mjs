2e7137ea85c36d7b2816dba35a6902c0434e39c9
nlnw <108184497+nlnw@users.noreply.github.com>
2024-01-14 15:28:05 -0500

    Astro.

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  integrations: [react()],
+  adapter: cloudflare(),
+  output: "hybrid",
+  build: {
+    assets: "static",
+  },
+});
